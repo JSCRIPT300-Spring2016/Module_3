ae22cb16d1444c4100604cdf89556c90a31b23d6
Andrew Reid <Andrew Reid>
2016-04-27 19:47:38 -0700

    working enhanced-date AR

--- enhanced-date.js
+++ enhanced-date.js
@@ -1,1 +1,113 @@
 // export this as a Node module using the code from Module_2
+
+'use strict';
+
+/* eslint-disable no-console */
+var privateDate = new Date(); // make this available to both get and set
+var dd = privateDate.getDate();
+var mm = privateDate.getMonth() + 1; //January is 0!
+var yyyy = privateDate.getFullYear();
+
+if (dd < 10) {
+  dd = '0' + dd;
+}
+if (mm < 10) {
+  mm = '0' + mm;
+}
+
+var checkDate = mm + dd + yyyy;
+var formatDate = mm + '/' + dd + '/' + yyyy;
+
+module.exports = {
+
+  setDate: function(newDate) {  // make a function for milli
+
+    if (privateDate == null) {
+      privateDate = new Date();
+      console.log(privateDate);
+      // Done - myDate.setDate() if no argument given
+    }
+
+    else {  // only using newMilli in use
+      privateDate = new Date();
+      console.log(privateDate);
+    }
+  },
+
+  getDate: function(dateAsk) {
+    if (dateAsk == undefined) {
+      console.log('No Args');
+      var n = privateDate.getTime();
+
+      return n;
+    }
+    else if (dateAsk == 'milliseconds') {
+      console.log('milliseconds');
+      var m = privateDate.getTime();
+      console.log(m);
+    }
+    else if (dateAsk == 'formatted') {
+      console.log(formatDate);
+
+      return(formatDate);
+    }
+  },
+
+  getDayName: function(dateAsk) {
+    var formatDay = privateDate.getDay();
+    var weekday = new Array(7);
+    weekday[0] = 'Sunday';
+    weekday[1] = 'Monday';
+    weekday[2] = 'Tuesday';
+    weekday[3] = 'Wednesday';
+    weekday[4] = 'Thursday';
+    weekday[5] = 'Friday';
+    weekday[6] = 'Saturday';
+
+    console.log(weekday[formatDay]);
+
+    return (weekday[formatDay]);
+  },
+
+  getMonthName: function(dateAsk) {
+    var formatMonth = privateDate.getMonth();
+    var monthNames = new Array(12);
+    monthNames[0] = 'January';
+    monthNames[1] = 'February';
+    monthNames[2] = 'March';
+    monthNames[3] = 'April';
+    monthNames[4] = 'May';
+    monthNames[5] = 'June';
+    monthNames[6] = 'July';
+    monthNames[7] = 'August';
+    monthNames[8] = 'September';
+    monthNames[9] = 'October';
+    monthNames[10] = 'November';
+    monthNames[11] = 'December';
+
+    console.log(monthNames[formatMonth]);
+
+    return (monthNames[formatMonth]);
+  },
+
+  isFuture: function(dateAsk) {
+
+    if (dateAsk <= checkDate) {
+      console.log('given date is not in the future');
+    }
+    else {
+      console.log('date is in the future');
+    }
+  },
+
+  isToday: function(dateAsk) {
+
+    if (dateAsk == checkDate) {
+      console.log('given date is today');
+    }
+    else {
+      console.log('given date is not today' + checkDate);
+    }
+  },
+
+};
