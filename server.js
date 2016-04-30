a3d424f3f4238a8ce8882735d77e787c32089aed
heidiflammable <heidiflammable@gmail.com>
2016-04-29 23:39:21 -0700

    uploading server file

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+//in progress
 
-// make this a simple http server that writes to the response stream object
+var http = require( 'http' );
+function handleRequest(request, response) {
+	response.writeHead(200, { 'Content-Type': 'text/plain' });
+	
+	var rightNow = new Date();
+	var rightNowAsString = rightNow.toDateString();
+	var lastIndex = rightNowAsString.lastIndexOf(' ');
+	var shortenedDate = rightNowAsString.substring(0, lastIndex);
+	
+	response.write('Today is ' + shortenedDate + ' Here are the available food trucks:');
+	//code for food trucks go here
 
-// the page should display the following message:
 
-// Today is <day name>, <month name> <date>. Here are the available food trucks:
+	response.end('Thanks! Response end');
+}.listen(3000, function () {
+	console.log('listening on port 3000');
+});
 
-// list the food trucks returned by filterByDay, passing in the current day name.
-// e.g. filterByDay(day); where "day" is determined using the enhanced-date module
-// The list of trucks returned will be an array of food truck objects. Iterate 
-// through the list, building up a string of food truck names. Once you're done 
-// iterating through that list, display the string you built up.
 
-// Remember that the response is a stream object that must be closed.
+
