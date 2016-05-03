86555d4346fa8f8b0c2799f72e2b8241694ef48b
Jason Goodwin <jgoodwin@sbgtv.com>
2016-05-03 11:19:50 -0700

    converted data util to a node module

--- enhanced-date.js
+++ enhanced-date.js
@@ -1,1 +1,72 @@
-// export this as a Node module using the code from Module_2
+'use strict';
+
+/**
+ * Expose `dateObj`.
+ */
+
+module.exports = dateObj;
+
+var dateObj = ( function(a) {
+  var currentDate;
+
+  function setDate(a) {
+
+    if (a instanceof Date){
+      currentDate = new Date(a);
+    } else if (isNaN(a) === false){
+      currentDate = new Date(a);
+    } else if (arguments.length === 0){
+      currentDate = new Date();
+    }
+  }
+
+  function getDate(f) {
+    if (arguments.length === 0){
+      return currentDate.getTime();
+    }
+
+    var format = f.format;
+
+    if (format === 'milliseconds') {
+      return currentDate.getTime();
+
+    } else if (format === 'formatted') {
+      return this.getMonthName() + ' ' + currentDate.getDate() + ', ' +
+      this.getYear();
+    }
+  }
+
+  function getDayName() {
+    return currentDate.toLocaleDateString('en-US', {weekday: 'long'});
+  }
+
+  function getMonthName() {
+    return currentDate.toLocaleDateString('en-US', {month: 'long'});
+  }
+
+  function isFuture() {
+    return currentDate > new Date();
+  }
+
+  function getYear() {
+    return currentDate.getFullYear();
+  }
+
+  function isToday() {
+    var candidate = new Date();
+
+    return (candidate.getYear() === currentDate.getYear() &&
+      (candidate.getMonth() === currentDate.getMonth()) &&
+        (candidate.getDay() === currentDate.getDay()));
+  }
+
+    return {
+    setDate: setDate,
+    getDate: getDate,
+    getDayName: getDayName,
+    getMonthName: getMonthName,
+    isFuture: isFuture,
+    getYear: getYear,
+    isToday: isToday
+  };
+})();
