e8e20bef50f3f599a48ce3c51427540045729ef2
heidiflammable <heidiflammable@gmail.com>
2016-05-04 00:26:30 -0700

    updating

--- enhanced-date.js
+++ enhanced-date.js
@@ -1,95 +1,101 @@
 // export this as a Node module using the code from Module_2
 'use strict';
 
-var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-    'August', 'September', 'October', 'November', 'December'];
-var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
-    'Thursday', 'Friday', 'Saturday'];
+var dateObj = (function() {
 
-var internalDate = null;
+  var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+      'August', 'September', 'October', 'November', 'December'];
 
-var dateInstantiate = function() {
-  if ( internalDate === null ) {
-    internalDate = new Date();
+  var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+      'Thursday', 'Friday', 'Saturday'];
 
-    return internalDate;
+  var internalDate = null;
+
+  function dateInstantiate() {
+    if ( internalDate === null ) {
+      internalDate = new Date();
+
+      return internalDate;
+    }
   }
-};
 
-var setDate = function( dateInput ) {
-  dateInstantiate();
+  function setDate( dateInput ) {
+    dateInstantiate();
+    if ( dateInput instanceof Date ) {
+      internalDate = new Date( dateInput );
 
-  if ( dateInput instanceof Date ) {
-    internalDate = new Date( dateInput );
+      return internalDate;
+    }
+    else if ( dateInput === undefined ) {
+      internalDate = new Date();
 
-    return internalDate;
+      return internalDate;
+    }
+    else{
+      internalDate = new Date( dateInput );
+
+      return internalDate;
+    }
   }
-  else if ( dateInput === undefined ) {
-    internalDate = new Date();
 
-    return internalDate;
+  function getDate( formatRequest ) {
+    dateInstantiate();
+
+    if ( formatRequest === undefined) {
+      return ( internalDate.getTime() );
+    }
+    else if ( formatRequest.format === 'formatted' ) {
+      return month [ internalDate.getMonth() ] + ' ' + internalDate.getDate() +
+       ', ' + internalDate.getFullYear();
+    }
+    else if ( formatRequest.format === 'milliseconds') {
+      return internalDate.getTime();
+    }
   }
-  else{
-    internalDate = new Date( dateInput );
 
-    return internalDate;
+  function getDayName() {
+    dateInstantiate();
+
+    return weekday[ internalDate.getDay() ];
   }
-};
 
-var getDate = function( formatRequest ) {
-  dateInstantiate();
+  function getMonthName() {
+    dateInstantiate();
 
-  if ( formatRequest === undefined) {
-    return ( internalDate.getTime() );
+    return month[ internalDate.getMonth() ];
   }
-  else if ( formatRequest.format === 'formatted' ) {
-    return month [ internalDate.getMonth() ] + ' ' + internalDate.getDate() +
-     ', ' + internalDate.getFullYear();
+
+  function isFuture() {
+    dateInstantiate();
+
+    if ( internalDate > Date.now() ) {
+      return true;
+    } else {
+      return false;
+    }
   }
-  else if ( formatRequest.format === 'milliseconds') {
-    return internalDate.getTime();
+
+  function isToday() {
+    dateInstantiate();
+    var rightNow = new Date();
+    var rightNowAsString = rightNow.toDateString();
+    var internalDateAsString = internalDate.toDateString();
+    if ( rightNowAsString === internalDateAsString ) {
+      return true;
+    } else {
+      return false;
+    }
   }
-};
 
-var getDayName = function() {
-  dateInstantiate();
+  return {
+    setDate : setDate,
+    getDate : getDate,
+    getDayName : getDayName,
+    getMonthName : getMonthName,
+    isFuture : isFuture,
+    isToday : isToday
+  };
 
-  return weekday[ internalDate.getDay() ];
-};
+})();
 
-var getMonthName = function() {
-  dateInstantiate();
-
-  return month[ internalDate.getMonth() ];
-};
-
-var isFuture = function() {
-  dateInstantiate();
-
-  if ( internalDate > Date.now() ) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-var isToday = function() {
-  dateInstantiate();
-  var rightNow = new Date();
-  var rightNowAsString = rightNow.toDateString();
-  var internalDateAsString = internalDate.toDateString();
-
-  if ( rightNowAsString === internalDateAsString ) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-exports.setDate = setDate;
-exports.getDate = getDate;
-exports.getDayName = getDayName;
-exports.getMonthName = getMonthName;
-exports.isFuture = isFuture;
-exports.isToday = isToday;
-
+module.exports = dateObj;
