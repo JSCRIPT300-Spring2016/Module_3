49fa9d990e91b1efea0a5feb98ceadc8aca7e16d
heidiflammable <heidiflammable@gmail.com>
2016-05-04 00:23:56 -0700

    updating server file

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-//in progress
-
+'use strict';
 var http = require( 'http' );
+var trucks = require('./trucks');
+var dateObj = require('./enhanced-date');
+var truckList = trucks.filterTrucksByDay(dateObj.getDayName());
+var date = new Date(dateObj.getDate( { format : 'milliseconds' } )).getDate();
+
 function handleRequest(request, response) {
-	response.writeHead(200, { 'Content-Type': 'text/plain' });
-	
-	var rightNow = new Date();
-	var rightNowAsString = rightNow.toDateString();
-	var lastIndex = rightNowAsString.lastIndexOf(' ');
-	var shortenedDate = rightNowAsString.substring(0, lastIndex);
-	
-	response.write('Today is ' + shortenedDate + ' Here are the available food trucks:');
-	//code for food trucks go here
-
-
-	response.end('Thanks! Response end');
-}.listen(3000, function () {
-	console.log('listening on port 3000');
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write('Today is ' + dateObj.getDayName() + ', ' +
+		dateObj.getMonthName() + ' ' + date+'. The food trucks available are:' +
+		'\n' + truckList);
+  response.end();
+}
+http.createServer(handleRequest).listen(3000, function () {
+  //console.log('listening on port 3000');
 });
-
-
-
